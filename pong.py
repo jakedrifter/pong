564742b6a9aefb038465907da89c8d0dac027d96
jakedrifter <142671011+jakedrifter@users.noreply.github.com>
2023-09-07 19:18:41 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,117 +1,136 @@
-#setup
 import pygame
 import math
 import random
+
 pygame.init()
-pygame.display.set_caption('pong')
-screen = pygame.display.set_mode((700, 500))
+pygame.display.set_caption("pong")
+screen = pygame.display.set_mode((650, 480))
 clock = pygame.time.Clock()
 running = True
 
-#variables
-line_width = 2
-paddle_width = 15
-paddle_height = 90
-paddle_r_x = screen.get_width() - paddle_width * 2
-paddle_r_y = (screen.get_height() / 2) - (paddle_height / 2)
-paddle_l_x = paddle_width * 2
-paddle_l_y = (screen.get_height() / 2) - (paddle_height / 2)
-paddle_speed = 8.5
 ball_size = 15
-ball_x = (screen.get_width() / 2) - (ball_size / 2)
-ball_y = (screen.get_height() / 2) - (ball_size / 2)
-ball_speed = 6
-ball_angle = random.randint(0,60) - 30
-ball_x_vel = (ball_speed * math.cos(math.radians(ball_angle)))
-ball_y_vel = (ball_speed * -math.sin(math.radians(ball_angle)))
+ball_speed = 12
+slow_ball_speed = 6
+ball_angle = random.randint(-35,35)
+paddle_width = 15
+paddle_height = 15 * 5
+paddle_speed = 8
 l_score = 0
 r_score = 0
+paddle_l_y_vel = 0
+line_width = 2
 
-
-#score
-font=pygame.font.Font(None,50)
 def l_score_text(l_score):
-   l_score_disp=font.render(str(l_score), 1,(255,255,255))
-   screen.blit(l_score_disp, ((screen.get_width() / 2 - 50), 10))
+   l_score_disp=pygame.font.Font(None,60).render(str(l_score), 1,(255,255,255))
+   screen.blit(l_score_disp, ((screen.get_width() / 4), 10))
 def r_score_text(r_score):
-   r_score_disp=font.render(str(r_score), 1,(255,255,255))
-   screen.blit(r_score_disp, ((screen.get_width() / 2 + 10), 10))
+   r_score_disp=pygame.font.Font(None,60).render(str(r_score), 1,(255,255,255))
+   screen.blit(r_score_disp, ((screen.get_width() / 1.5), 10))
 
-#loop
+# sprite classes
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((ball_size,ball_size))
+        self.rect = self.image.get_rect()
+        pygame.draw.rect(self.image, "white", pygame.Rect(0, 0, ball_size, ball_size))
+        self.rect.x = x
+        self.rect.y = y
+        self.x_vel = (slow_ball_speed * math.cos(math.radians(ball_angle)))
+        self.y_vel = (slow_ball_speed * math.sin(math.radians(ball_angle)))
+    def update(self):
+        self.rect.x += self.x_vel
+        self.rect.y += self.y_vel
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((paddle_width, paddle_height))
+        self.rect = self.image.get_rect()
+        pygame.draw.rect(self.image, "white", pygame.Rect(0, 0, paddle_width, paddle_height))
+        self.rect.x = x
+        self.rect.y = y
+    def move(self,y):
+        if self.rect.y >= 0 and self.rect.y + paddle_height <= screen.get_height():
+            self.rect.y += y
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y + paddle_height > screen.get_height():
+            self.rect.y = screen.get_height() - paddle_height
+    
+# sprite groups
+ball = Ball((screen.get_width() / 2) - (ball_size / 2),(screen.get_height() / 2) - (ball_size / 2))
+ball_group = pygame.sprite.Group(ball)
+paddle_l = Paddle(paddle_width * 2,(screen.get_height() / 2) - (paddle_height / 2))
+paddle_r = Paddle(screen.get_width() - (paddle_width * 2),(screen.get_height() / 2) - (paddle_height / 2))
+paddle_group = pygame.sprite.Group(paddle_l,paddle_r)
+
+# game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    #key presses
+    ball_group.update()
+    paddle_group.update()
+
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and paddle_r_y > 0:
-        paddle_r_y -= paddle_speed
-    if keys[pygame.K_DOWN] and paddle_r_y + paddle_height < screen.get_height():
-        paddle_r_y += paddle_speed
-    if keys[pygame.K_w] and paddle_l_y > 0:
-        paddle_l_y -= paddle_speed
-    if keys[pygame.K_s] and paddle_l_y + paddle_height < screen.get_height():
-        paddle_l_y += paddle_speed
-    
-    #see if ball hits left paddle
-    if ball_x <= paddle_l_x + paddle_width and ball_x >= paddle_l_x - (paddle_width) and ball_y + ball_size > paddle_l_y and ball_y < paddle_l_y + paddle_height:
-        ball_speed = 11
-        ball_x_vel = (ball_speed * math.cos(math.radians(((ball_y + (ball_size / 2)) - (paddle_l_y + (paddle_height / 2))))))
-        ball_y_vel = -(ball_speed * -math.sin(math.radians(((ball_y + (ball_size / 2)) - (paddle_l_y + (paddle_height / 2))))))
+    if keys[pygame.K_UP]:
+        paddle_r.move(-paddle_speed)
+    if keys[pygame.K_DOWN]:
+        paddle_r.move(paddle_speed)
 
-    #see if ball hits right paddle
-    if ball_x + ball_size >= paddle_r_x and ball_x + ball_size <= paddle_r_x + (paddle_width) and ball_y + ball_size > paddle_r_y and ball_y < paddle_r_y + paddle_height:
-        ball_speed = 11
-        ball_x_vel = -(ball_speed * math.cos(math.radians(((ball_y + (ball_size / 2)) - (paddle_r_y + (paddle_height / 2))))))
-        ball_y_vel = -(ball_speed * -math.sin(math.radians(((ball_y + (ball_size / 2)) - (paddle_r_y + (paddle_height / 2))))))
-    
-    #see if left paddle missed
-    if ball_x < 0 - ball_size:
-        ball_speed = 6
-        ball_angle = random.randint(0,60) - 30
-        ball_x_vel = (ball_speed * math.cos(math.radians(ball_angle)))
-        ball_y_vel = (ball_speed * -math.sin(math.radians(ball_angle)))
-        ball_x = (screen.get_width() / 2) - (ball_size / 2)
-        ball_y = (screen.get_height() / 2) - (ball_size / 2)
-        paddle_l_y = (screen.get_height() / 2) - (paddle_height / 2)
-        paddle_r_y = (screen.get_height() / 2) - (paddle_height / 2)
+    for paddle in pygame.sprite.spritecollide(ball,paddle_group,False):
+        left = (ball.rect.x + ball_size) - paddle.rect.x
+        right = (paddle.rect.x + paddle_width) - ball.rect.x
+        bottom = (paddle.rect.y + paddle_height) - ball.rect.y
+        top = (ball.rect.y + ball_size) - paddle.rect.y
+        place = (ball.rect.y + (ball_size / 2)) - (paddle.rect.y + (paddle_height / 2))
+        if left < right and left < top and left < bottom:
+            ball.x_vel = -(ball_speed * math.cos(math.radians((place) * 1.3)))
+            ball.y_vel = (ball_speed * math.sin(math.radians((place) * 1.3)))
+        if right < left and right < top and right < bottom:
+            ball.x_vel = (ball_speed * math.cos(math.radians((place) * 1.3)))
+            ball.y_vel = (ball_speed * math.sin(math.radians((place) * 1.3)))
+        if top < bottom and top < left and top < right and ball.y_vel > 0:
+            ball.y_vel *= -1
+        if bottom < top and bottom < left and bottom < right and ball.y_vel < 0:
+            ball.y_vel *= -1
+    if ball.rect.y < 0 or ball.rect.y + ball_size > screen.get_height():
+        ball.y_vel *= -1
+    if ball.rect.x + ball_size < 0:
+        ball.x_vel = (slow_ball_speed * math.cos(math.radians(random.randint(-30,30))))
+        ball.y_vel = (slow_ball_speed * math.sin(math.radians(random.randint(-30,30))))
+        ball.rect.x = (screen.get_width() / 2) - (ball_size / 2)
+        ball.rect.y = (screen.get_height() / 2) - (ball_size / 2)
         r_score += 1
-
-    #see if right paddle missed
-    if ball_x > screen.get_width():
-        ball_speed = 6
-        ball_angle = random.randint(0,60) - 30
-        ball_x_vel = -(ball_speed * math.cos(math.radians(ball_angle)))
-        ball_y_vel = -(ball_speed * -math.sin(math.radians(ball_angle)))
-        ball_x = (screen.get_width() / 2) - (ball_size / 2)
-        ball_y = (screen.get_height() / 2) - (ball_size / 2)
-        paddle_r_y = (screen.get_height() / 2) - (paddle_height / 2)
-        paddle_l_y = (screen.get_height() / 2) - (paddle_height / 2)
+    if ball.rect.x > screen.get_width():
+        ball.x_vel = -(slow_ball_speed * math.cos(math.radians(random.randint(-30,30))))
+        ball.y_vel = (slow_ball_speed * math.sin(math.radians(random.randint(-30,30))))
+        ball.rect.x = (screen.get_width() / 2) - (ball_size / 2)
+        ball.rect.y = (screen.get_height() / 2) - (ball_size / 2)
         l_score += 1
-    
-    #see if ball hit top or bottom of screen
-    if ball_y <= 0 or ball_y >= screen.get_height() - ball_size:
-        ball_y_vel *= -1
 
-    #computer controls for left paddle. delete this code for 2 player mode.
-    if ball_y + (ball_size / 2) + 30 < paddle_l_y + (paddle_height / 2) and paddle_l_y > 0 and ball_x_vel < 0:
-        paddle_l_y -= paddle_speed * .7
-    if ball_y + (ball_size / 2) - 30 > paddle_l_y + (paddle_height / 2) and paddle_l_y + paddle_height < screen.get_height() and ball_x_vel < 0:
-        paddle_l_y += paddle_speed * .7
-    
-    #add ball vectors to ball coords
-    ball_x += ball_x_vel
-    ball_y += ball_y_vel
+    paddle_l_y_vel *= .8
+    paddle_l.rect.y += paddle_l_y_vel
+    if ball.rect.y + (ball_size / 2) + 26 < paddle_l.rect.y + (paddle_height / 2) and ball.x_vel < 0:
+        paddle_l_y_vel -= 2.2
+    if ball.rect.y + (ball_size / 2) - 26 > paddle_l.rect.y + (paddle_height / 2) and ball.x_vel < 0:
+        paddle_l_y_vel += 2.2
+    if paddle_l.rect.y < 0:
+        paddle_l_y_vel = 0
+        paddle_l.rect.y = 0
+    if paddle_l.rect.y + paddle_height > screen.get_height():
+        paddle_l_y_vel = 0
+        paddle_l.rect.y = screen.get_height() - paddle_height
 
-    #render
-    screen.fill("black")
+    # render
+    screen.fill("#212325")
     pygame.draw.rect(screen, "white", pygame.Rect((screen.get_width() / 2) - (line_width / 2), 0, line_width, screen.get_height()))
-    pygame.draw.rect(screen, "white", pygame.Rect(ball_x,ball_y, ball_size, ball_size))
+    ball_group.draw(screen)
+    paddle_group.draw(screen)
     l_score_text(l_score)
     r_score_text(r_score)
-    pygame.draw.rect(screen, "white", pygame.Rect(paddle_r_x,paddle_r_y, paddle_width, paddle_height))
-    pygame.draw.rect(screen, "white", pygame.Rect(paddle_l_x,paddle_l_y, paddle_width, paddle_height))
     pygame.display.flip()
     clock.tick(60) / 1000
 pygame.quit()
